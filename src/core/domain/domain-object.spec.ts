128f2eef1b7a0ad30a5f2ab572eeaade7bac7776
Abner Machado <54149914+abner81@users.noreply.github.com>
2024-10-04 23:57:37 -0300

    test: add complete test domainObject

--- src/core/domain/domain-object.spec.ts
+++ src/core/domain/domain-object.spec.ts
@@ -9,13 +9,33 @@ describe('DomainObject', () => {
 
   it('should export to correct values', () => {
     const props = { value: 3 };
-    const exported = makeSUTNumber({ value: 3 }).export();
+    const exported = makeSUTNumber(props).export();
 
     expect(exported).not.toBe(props);
     expect(exported).toStrictEqual(props);
   });
 
-  it('should ', () => {
+  it('should toString() with correct values', () => {
+    const pNumber = makeSUTNumber({ value: 4 });
+    const pString = new StringValueObject({ value: 'My String' });
+    const pBoolean = new BooleanValueObject({ value: true });
+    const pNull = new NullValueObject({ value: null });
+    const pUndefined = new UndefinedValueObject({ value: undefined });
+
+    const sNumber = String(pNumber);
+    const sString = String(pString);
+    const sBoolean = String(pBoolean);
+    const sNull = String(pNull);
+    const SUndefined = String(pUndefined);
+
+    expect(sNumber).toBe('4');
+    expect(sString).toBe('My String');
+    expect(sBoolean).toBe('true');
+    expect(sNull).toBe('null');
+    expect(SUndefined).toBe('undefined');
+  });
+
+  it('should call parseAndStore() and parse() on instance Class', () => {
     const parseAndStoreSpy = jest.spyOn(
       NumberValueObject.prototype as any,
       'parseAndStore',
@@ -26,6 +46,33 @@ describe('DomainObject', () => {
 
     expect(parseAndStoreSpy).toHaveBeenCalledWith(props);
     expect(parseSpy).toHaveBeenCalledWith(props);
+  });
+
+  it('should transform toJSON() with equal values an export()', () => {
+    const props = { value: 3 };
+    const sut = makeSUTNumber(props);
+    const exported = sut.export();
+    const toJSON = JSON.parse(JSON.stringify(sut));
+
+    expect(toJSON).toStrictEqual(exported);
+  });
+
+  it('should throw error if not receive object in params', () => {
+    const act = () => makeSUTNumber('not object' as unknown as NumberProps);
+    expect(act).toThrow(DomainException);
+  });
+
+  it('should throw error if parse() isnt success', () => {
+    const errorMessage = 'My Error Message';
+    class SUT extends NumberValueObject {
+      override parse(props: NumberProps): number {
+        throw new DomainException(errorMessage);
+      }
+    }
+
+    const sut = () => new SUT({ value: 3 });
+    expect(sut).toThrow(DomainException);
+    expect(sut).toThrow(errorMessage);
   });
 
   // ---------------------------------------------------
@@ -44,6 +91,70 @@ describe('DomainObject', () => {
     }
 
     export(): Required<NumberProps> {
+      return { value: this.state };
+    }
+  }
+
+  type StringProps = { value: string };
+  class StringValueObject extends DomainObject<StringProps, string> {
+    get value() {
+      return this.state;
+    }
+
+    protected parse(props: StringProps): string {
+      const { value } = props;
+      return value;
+    }
+
+    export(): Required<StringProps> {
+      return { value: this.state };
+    }
+  }
+
+  type BooleanProps = { value: boolean };
+  class BooleanValueObject extends DomainObject<BooleanProps, boolean> {
+    get value() {
+      return this.state;
+    }
+
+    protected parse(props: BooleanProps): boolean {
+      const { value } = props;
+      return value;
+    }
+
+    export(): Required<BooleanProps> {
+      return { value: this.state };
+    }
+  }
+
+  type NullProps = { value: null };
+  class NullValueObject extends DomainObject<NullProps, null> {
+    get value() {
+      return this.state;
+    }
+
+    protected parse(props: NullProps): null {
+      const { value } = props;
+      return value;
+    }
+
+    export(): Required<NullProps> {
+      return { value: this.state };
+    }
+  }
+
+  type UndefinedProps = { value: undefined };
+  class UndefinedValueObject extends DomainObject<UndefinedProps, undefined> {
+    get value() {
+      return this.state;
+    }
+
+    protected parse(props: UndefinedProps): undefined {
+      const { value } = props;
+      return value;
+    }
+
+    export(): Required<UndefinedProps> {
       return { value: this.state };
     }
   }
